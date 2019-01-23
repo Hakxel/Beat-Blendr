755407cca7ef95e4a13c4bf4de8f304b76473a1f
Hakxel <asiel_montes@yahoo.com>
2019-01-23 07:36:50 -0500

    Add location to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_212559) do
+ActiveRecord::Schema.define(version: 2019_01_23_121120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2019_01_22_212559) do
     t.json "info"
     t.string "image"
     t.string "token"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
