a8b946acdc18cc6a9cbccdf50849e1551013d01f
Alvaro Vargas <vargasa910@gmail.com>
2019-02-03 12:05:43 -0500

    changed artist word cloud to preview artists

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,18 @@
 class PagesController < ApplicationController
   def home
+
     if user_signed_in?
       @playlist = current_user.playlist
-      @artists  = current_user.tracks.pluck(:artist).uniq.shuffle
+      artists = []
+      User.near([current_user.latitude, current_user.longitude], 0.25)
+          .each do |user|
+            user.tracks.pluck(:artist).each do |artist|
+              artists << artist
+            end
+      @artists  = artists.uniq.shuffle
+      end
     end
   end
-
   def show
   end
 end
