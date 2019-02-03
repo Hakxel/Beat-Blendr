8cf380f3d0644ab4ea8eae055cbcccd0e7f51c80
AVargas910 <45184205+AVargas910@users.noreply.github.com>
2019-02-03 15:36:52 -0500

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,13 +4,12 @@ class PagesController < ApplicationController
     if user_signed_in?
       @playlist = current_user.playlist
       artists = []
-      User.near([current_user.latitude, current_user.longitude], 0.25)
-          .each do |user|
-            user.tracks.pluck(:artist).each do |artist|
-              artists << artist
-            end
+      User.near([current_user.latitude, current_user.longitude], 0.25).each do |user|
+        user.tracks.pluck(:artist).each do |artist|
+          artists << artist
+        end      
+      end      
       @artists  = artists.uniq.shuffle
-      end
     end
   end
   def show
